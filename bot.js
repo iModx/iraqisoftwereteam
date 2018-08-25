460c8f94eca35390ec5ebfa94cfd0b7c16089315
iModx <42695512+iModx@users.noreply.github.com>
2018-08-25 21:02:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -361,4 +361,25 @@ client.on('message', message => {
 
 
 
+
+client.on('message', message => {
+   if (message.content === ".id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+
+
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
