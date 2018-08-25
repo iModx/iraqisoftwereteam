b53c66ded529da1a6061e561057e3661ee1a8434
iModx <42695512+iModx@users.noreply.github.com>
2018-08-25 17:10:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,6 +69,28 @@ client.on('message', function(msg) {
 
 
 
+client.on('message', message => {
+    if (message.content.startsWith(".bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO IST Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``Servers``', [client.guilds.size], true)
+            .addField('``Channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[ . ]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                  .setFooter('By | ISTeam')
+    })
+}
+});
+
 
 
 
