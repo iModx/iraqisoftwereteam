d7a7321c48fa745fc2d4cd409438f4d0b5038cd8
iModx <42695512+iModx@users.noreply.github.com>
2018-08-25 16:31:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,43 +32,11 @@ client.user.setGame(`IST Server`,"http://twitch.tv/S-F")
 
 
 
-
-
-
-
-
-
-const d = require("discord.js");
-const client = new d.Client();
-var prefix ="."
-var colors = new Array(100);
-
-client.on("message", (message) => {
-    var command = message.content.split(" ")[0].slice(prefix.length);
-    switch(command) {
-        case "cc" :
-        if (!message.channel.type == "text") return;
-        if (!message.member.hasPermission("MANAGE_ROLES")) return
-        for(var x = 0;x<colors.length;x++){
-            message.guild.createRole({name : x, color : colors[x]});
-        };
-    };
-}).login("NDgyODkxNDQ3NjUwMjIyMDgw.DmLfUw.Qc_cFJfLcwEBwu-InQ6vLperq_M");
-function sin_to_hex(i, phase) {
-    var sin = Math.sin(Math.PI / size * 2 * i + phase);
-    var int = Math.floor(sin * 127) + 128;
-    var hex = int.toString(16);
-  
-    return hex.length === 1 ? '0'+hex : hex;
-};
-for(var x=0;x<colors.length;x++) {
-    let r   = sin_to_hex(i, 0 * Math.PI * 2/3);
-    let b  = sin_to_hex(i, 1 * Math.PI * 2/3);
-    let g = sin_to_hex(i, 2 * Math.PI * 2/3);
-    colors[x] = '#'+ r + g + b;
-};
-
-
+client.on('message' , message => {
+if(message.content === 'ovoice') {
+    message.channel.send(`**عدد الاشخاص الموجودين بـ  الرومات الصوتيه : ${message.guild.members.filter(g => g.voiceChannel).size}**`);
+}
+});
 
 
 
